4e56fe013ade547df01eb3d8a195d33787598881
vxanthio <basiliki.martios1@gmail.com>
2026-05-28 15:10:29 +0300

    feat(search): add field selector for name, race, gender, alignment

--- script/search.js
+++ script/search.js
@@ -1,15 +1,22 @@
 import { updateState } from "./state.js";
 import { renderApp  } from "./render.js";
 
-export const filterHeroes=(heroes, query)=>{
-    const result=heroes.filter(hero=>hero.name.toLowerCase().includes(query.toLowerCase()))
-return result
+export const filterHeroes=(heroes, query,field)=>{
+    return heroes.filter(hero=>{
+    if (field==="name") return hero.name.toLowerCase().includes(query.toLowerCase())
+if (field==="race") return hero.appearance.race.toLowerCase().includes(query.toLowerCase())
+if (field==="gender") return hero.appearance.gender.toLowerCase().includes(query.toLowerCase())
+if (field==="alignment") return hero.biography.alignment.toLowerCase().includes(query.toLowerCase())
+else return hero.name.toLowerCase().includes(query.toLowerCase())
+})
 }
 export const initSearch =(heroes) =>{
     const searchInput = document.getElementById("search")
+    const selectField=document.getElementById("field")
     searchInput.addEventListener('input',(event)=>{
 const query =event.target.value
-const filteredHeroes=filterHeroes(heroes, query)
+const field=selectField.value
+const filteredHeroes=filterHeroes(heroes, query,field)
 updateState({query})
 renderApp()
     })
